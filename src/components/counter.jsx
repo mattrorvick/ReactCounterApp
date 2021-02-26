63177c02461819151f303340dcb8bc110f333ff8
mattrorvick <matthewrorvick@gmail.com>
2021-02-26 11:06:37 -0600

    handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,22 +5,33 @@ class Counter extends Component {
     count: 0,
   };
 
-  styles = {
-    fontWeight: "bold",
-  };
+  handleIncrement() {
+    console.log("Increment Clicked", this);
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
