125d61995d36062f1cb0753e87b26e9dcc667bd7
mattrorvick <matthewrorvick@gmail.com>
2021-02-27 14:47:01 -0600

    updating navbar to count

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
